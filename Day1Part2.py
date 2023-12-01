414bfdae8e6fe9c2cd7de6dcd1cabe8252e49be5
andykocsis <150318802+andykocsis@users.noreply.github.com>
2023-12-01 15:39:32 -0500

    Add files via upload

---
+++ Day1Part2.py
@@ -0,0 +1,34 @@
+import re
+
+rawdata = []
+combined_num = []
+
+#Read data from day1.txt into list rawdata
+with open("day1.txt") as day1:
+	for line in day1:
+		line = line.strip()
+		rawdata.append(line)
+		
+for x in rawdata:
+	
+#Search for first digit
+	match = re.search('\d', str(x))
+	first_digit = int(match.group())
+	
+	
+#Search for last digit in original string
+	backmatch = re.search('\d', x [::-1])
+	second_digit = int(backmatch.group())
+	
+	
+#Combine first and last digits
+	combined_num.append(first_digit*10+second_digit)
+
+total = sum(combined_num)
+print(total)
+
+	
+
+	
+
+	
